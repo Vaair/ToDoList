a3e69a0e1820eaf4e6ee5467884f8a93afdaceda
Vaair <valeria.tarasenko.a@gmail.com>
2020-09-14 19:12:25 +0300

    save to coreData

--- ToDoList/TableViewController.swift
+++ ToDoList/TableViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
     
-    var tasks = [String]()
+    var tasks: [Task] = [] //чтобы не было ошибки удаляем в локейшн directed data и перезагружаем проект
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +27,9 @@ class TableViewController: UITableViewController {
         let alertController = UIAlertController(title: "New Task", message: "Please add a new task", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { action in
             let tf = alertController.textFields?.first
-            if let newTask = tf?.text {
-                self.tasks.insert(newTask, at: 0)
+            if let newTaskTitle = tf?.text {
+                //self.tasks.insert(newTask, at: 0)
+                self.saveTask(withTitle: newTaskTitle)
                 self.tableView.reloadData()
             }
         }
@@ -40,6 +42,21 @@ class TableViewController: UITableViewController {
         present(alertController, animated:  true, completion: nil )
     }
     
+    private func saveTask(withTitle title: String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext //добрались до контекста
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        
+        let taskObject = Task(entity: entity, insertInto: context)
+        taskObject.title = title
+        
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -54,8 +71,8 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = tasks[indexPath.row]
-        // Configure the cell...
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
         
         return cell
     }
